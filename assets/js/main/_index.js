1610c29460a01056e4f37756d7445b5b281ab281
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-10-29 19:44:31 +0000

    Solve the Kata number 003

--- assets/js/main/_index.js
+++ assets/js/main/_index.js
@@ -107,8 +107,6 @@ function readZalgo(zalgotext) {
 
 
 // TESTS
-
-
 console.log(readZalgo("H̗̪͇͓̙͎̣̄ͬa͚̯̦͉̖̥v͆ͩ̃͆̓̐ͥe̟͎͖͕͍̎ ̰͚̩̟͕̰͊̽̍ͯ̌͊ā̖̪͉͍̥͙̿ͩ̃ͅ ̬̥͎͑̿ͧg̰̳̺͔̦͉ͫ̀̐̓̐r̝̫̱̘̰͐͋ͯͭͭͭ͆e͙͕̖̗͙̰͌ͭä͓͚̝͓́̌͑ͪ͊ṱͥ ̱ͣd͎͔͎͇̫̪̘̃͐̇à͕̮̈͋ͪy̼̳̱ͮ!̳̥̰̭͇̔ͮ̽̓")) //  "Have a great day!"
 
 
