4a4ac485c92188af0f7ba202638d13d47b7077cf
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-10-30 19:55:55 +0000

    Add a Kata number 005

--- assets/js/main/_index.js
+++ assets/js/main/_index.js
@@ -166,7 +166,7 @@ Check Eloquent Javascript p176
 // ES6
 // const heggeleggleggo = word => [...word].map( letter => /[bcdfghjklmnpqrstvxzwy]/i.test(letter) ? `${letter}egg` : letter).join('')
 
-// Very short 
+// Very short
 let heggeleggleggo = w => w.replace(/[bcdfghjklmnpqrstvwxyz]/ig, "$&egg")
 
 
@@ -176,3 +176,75 @@ console.log(heggeleggleggo("code here")) //"ceggodegge heggeregge"
 console.log(heggeleggleggo("egg")) //"egegggegg"
 
 
+//──── 005 ───────────────────────────────────────────────────────────────────────────────
+console.log('%c Kata 005 ', 'color: red');
+/*
+
+It's Friday night, and Chuck is bored.
+He's already run 1,000 miles, stopping only to eat a family sized bag of Heatwave Doritos
+and a large fistful of M&Ms. He just can't stop thinking about kicking something!
+
+There is only one thing for it,
+Chuck heads down to his local MMA gym and immediately challenges every fighter
+there to get in the cage and try and take him down... AT THE SAME TIME!
+
+You are provided an array of strings that represent the cage and Chuck's opponents.
+Your task, in traditional Chuck style, is to take their heads off!! Throw punches, kicks,
+headbutts (or more likely - regex or iteration...) but whatever you do, remove their heads.
+Return the same array of strings, but with the heads ('O') removed and replaced with a space (' ').
+
+If the provided array is empty, or is an empty string, return 'Gym is empty'.
+If you are given an array of numbers, return 'This isn't the gym!!'.
+
+FIGHT!!
+
+Original design of this kata was a much more beautiful thing - the test cases illustrate the idea,
+and the intended output.
+I am unable to make the actual output go over multiple lines so for now at least you will have to
+imagine the beauty!
+
+*/
+// ES5
+function headSmash(array) {
+
+}
+
+// TESTS
+console.log(headSmash(
+[
+'*****************************************',
+'**  _O_   *   _O_   *   _O_   *   _O_  **',
+'** C(.)J  *  /(.)J  *  /(.)J  *  C(.)J **',
+'** _/ |_  *  _| |_  *  _( )_  *  _/ )_ *']))
+
+console.log(headSmash(
+[
+'***  _O_   **   _O_   **   _O_   **   _O_  ***',
+'*** /(.)J  **  C(.)J  **  /(.)J  **  /(.)J ***',
+'*** _| |_  **  _| |_  **  _( )_  **  _/ |_ **']))
+
+console.log(headSmash(
+[
+'*****************************************',
+'***********   _O_   *   _O_   ***********',
+'**  _O_   *  /(.)J  *  /(.)J  *   _O_  **',
+'** /(.)J  *  _| |_  *  _( )_  *  /(.)J **',
+'** _( )_  *********************  _( )_ **',
+'******************* X ******************']))
+
+console.log(headSmash(
+[
+'*****************************************',
+'***********   _O_   *   _O_   ***********',
+'**  _O_   *  C(.)J  *  /(.)J  *   _O_  **',
+'** /(.)J  *  _| |_  *  _/ )_  *  C(.)J **',
+'** _/ )_  *********************  _/ |_ **',
+'******************* X *******************',
+'**********************  _O_  ************',
+'**  _O_   *   _O_   *  /(.)J  *   _O_  **',
+'** /(.)J  *  C(.)J  *  _/ )_  *  C(.)J **',
+'** _( )_  *  _| |_  ***********  _/ |_ **',
+'****************************************']))
+
+
+console.log(headSmash(8764646)) // 'This isn\'t the gym!!');
